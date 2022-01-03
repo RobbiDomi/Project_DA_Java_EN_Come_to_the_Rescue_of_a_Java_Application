34ce957e21cf47348a61fdbd14223a82a51d7f6c
RobbiDomy <robdom@hotmail.fr>
2022-01-03 18:50:42 +0100

    1st part / program

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,37 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import com.hemebiotech.analytics.ReadSymptomDataFromFile;
+import java.io.*;
+import java.util.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		Path path = Paths.get(System.getProperty("user.dir"))
+				.resolve("Project02Eclipse/symptoms.txt");
 
-			line = reader.readLine();	// get another symptom
+		Map<String, Integer> map = new HashMap<>();
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(path.toFile()));
+			String line = reader.readLine();
+			while (line != null) {
+				if (map.containsKey(line)) {
+					map.put(line, map.get(line) + 1);
+				} else {
+					map.put(line, 1);
+				}
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		System.out.println(map);
 	}
-}
+	}
+
