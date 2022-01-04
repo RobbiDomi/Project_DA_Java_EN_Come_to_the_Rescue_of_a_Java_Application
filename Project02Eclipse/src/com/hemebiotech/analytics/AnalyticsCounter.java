c13636f74b65a95771661b0fbbc33dbb984f8bc3
RobbiDomy <robdom@hotmail.fr>
2022-01-04 19:39:33 +0100

    Program Analytics Counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,35 +3,92 @@ package com.hemebiotech.analytics;
 import com.hemebiotech.analytics.ReadSymptomDataFromFile;
 import java.io.*;
 import java.util.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 
 public class AnalyticsCounter {
 
-	public static void main(String args[]) throws Exception {
+	/**
+	 * @summary  writing fontion the symptoms's occurences
+	 *
+	 */
 
-		Path path = Paths.get(System.getProperty("user.dir"))
-				.resolve("Project02Eclipse/symptoms.txt");
-
-		Map<String, Integer> map = new HashMap<>();
-
+	public static void writeResult(Map<String, Integer> map) {
+		// TODO write to the result.txt map
+	   /**
+	    * @param  FileWriter to create file to load result
+	    *
+	    */
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(path.toFile()));
+			FileWriter fw = new FileWriter("Project02Eclipse/result.txt");
+			BufferedWriter bw = new BufferedWriter(fw);
+
+			// parcourir la map de String
+			for (String symptom : map.keySet()) {
+				bw.write(symptom + " : " + map.get(symptom) + "\n");
+			}
+			bw.close();
+		/**
+		* @return storing symptoms's occurences
+		*
+		*/
+
+		}
+		catch (Exception e) {
+			e.getStackTrace();
+		}
+	}
+	/**
+	 * @summary  creating the function to count symptoms's occurrences
+	 *
+	 */
+
+	public static Map<String, Integer> readFile() {//Fonction
+
+		File occurence = new File("Project02Eclipse/symptoms.txt");
+					// creating hashmap to store the symptoms's occurences
+		Map<String, Integer> map = new HashMap<>();
+					// creating a BufferReader to read the file symptoms.txt
+		/**
+		 * @param  BufferedReader to read the symptoms's file.txt
+		 *
+		 */
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(occurence));
 			String line = reader.readLine();
 			while (line != null) {
+					//storing the different symptoms and occurrences
 				if (map.containsKey(line)) {
 					map.put(line, map.get(line) + 1);
+					//implementing the number of symptoms in the map
 				} else {
 					map.put(line, 1);
 				}
+					// reading another line on the symptoms's files
 				line = reader.readLine();
 			}
+					// closing the BufferReader
 			reader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return map;
+		/**
+		 * @return returning the map/symptoms for use in other functions
+		 *
+		 */
+
+
+	}
+	/**
+	 * @summary  priting the result
+	 *
+	 */
+	public static void main(String args[])  {
+				//retrieving Hashmap's readfile information
+		Map<String, Integer> map = readFile();
+				//displaying the occurrence of elements
 		System.out.println(map);
+		writeResult(map);
 	}
-	}
+}
 
